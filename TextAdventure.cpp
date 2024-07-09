b71e7057ea5c3a4eb9c81e407358d562fc05e7d1
DonJon6062 <144158458+DonJon6062@users.noreply.github.com>
2024-07-08 23:13:06 -0700

    Add files via upload

--- TextAdventure.cpp
+++ TextAdventure.cpp
@@ -4,11 +4,19 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
-using std::endl;
+//using std::endl;
 using namespace std;
 
 //the signifier of your limited tries
-int outOfTime = 3;
+int outOfTime = 3; //global value int 
+bool day1 = false; 
+bool day2 = false;
+bool day3 = false;
+bool day4 = false;
+bool day5 = false;
+bool day6 = false;
+bool day7 = false;
+
 
 //an extensive list of the acts structure
 void beginning();
@@ -21,72 +29,74 @@ void daySix();
 void daySeven();
 void Finale();
 
-void check();
-void choice();
+void check(); //small function that shows global int
+void choice(); //the meat and potatoes of choices
 
 void check() 
 {
-	cout << outOfTime << endl;
-	choice();
+	cout << outOfTime << endl; //accesses global variable
+	choice(); //goes back to the main function
 }
 
 void choice() 
 {
-	char select;
-	int daySelect;
+	char select; //local vars
+	int daySelect; //for choices made only here
 
-	cout << "Use 'C' to check those charges, and 'T' to traverse the days. Uppercase, please!" << endl;
-
-	cin >> select;
-	if (select == 'C')
+	if (outOfTime == 0)
 	{
-		check();
+		Finale(); //checks whether or not the final function should be run first
 	}
-	else if (select == 'T')
+	else 
 	{
-		cout << "How many days back? 1-7; Pick 1 for yesterday, and 7 for the beginning of the week." << endl;
-		cin >> daySelect;
-		if (outOfTime >= 0)
+		cout << "Use 'C' to check those charges, and 'T' to traverse the days. Uppercase, please!" << endl; //instructions
+		cin >> select; //gets a letter variable
+		if (select == 'C')
 		{
-			outOfTime -= 1;
+			check(); //runs the smallest function
 		}
-		if (daySelect == 1)
+		else if (select == 'T')
 		{
-			dayOne();
+			cout << "How many days back? 1-7; Pick 1 for yesterday, and 7 for the beginning of the week." << endl; //instructions
+			cin >> daySelect; //aptly named functions and variable
+			if (daySelect == 1)
+			{
+				dayOne();
+			}
+			if (daySelect == 2)
+			{
+				dayTwo();
+			}
+			if (daySelect == 3)
+			{
+				dayThree();
+			}
+			if (daySelect == 4)
+			{
+				dayFour();
+			}
+			if (daySelect == 5)
+			{
+				dayFive();
+			}
+			if (daySelect == 6)
+			{
+				daySix();
+			}
+			if (daySelect == 7)
+			{
+				daySeven();
+			}
 		}
-		if (daySelect == 2)
+		else //in the case of a misclick for the char
 		{
-			dayTwo();
-		}		
-		if (daySelect == 3)
-		{
-			dayThree();
-		}		
-		if (daySelect == 4)
-		{
-			dayFour();
-		}		
-		if (daySelect == 5)
-		{
-			dayFive();
-		}		
-		if (daySelect == 6)
-		{
-			daySix();
-		}		
-		if (daySelect == 7)
-		{
-			daySeven();
+			cout << "Invalid! You are using uppercases, yeah?" << endl;
 		}
-	}
-	else
-	{
-		cout << "Invalid! You are using uppercases, yeah?" << endl;
 	}
 }
-void beginning()
+void beginning() //textdump that runs first
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 	}
@@ -105,9 +115,9 @@ void beginning()
 	}
 }
 
-void dayOne() 
+void dayOne() //if one is seleted; subtracts from the global variable once it has run, bday card
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -118,14 +128,19 @@ void dayOne()
 		cout << "Time traveller you was rummaging through present you's own belongings until present you walked in, then time traveller you let out a shrill cry before dissappearing." << endl;
 		cout << "You were kind of scared after that. Who wouldn't be? Seeing another you scream and become nothing." << endl;
 		cout << "Now, the time traveler rather than the time travel..-ee? That was funny, to scare yourself. You do it again when past you walks in." << endl;
-		cout << "Unfortunately, nothing of note other than /a/ note, giving the name of your friend, Francis. Not enough info yet.." << endl;
+		cout << "Unfortunately, nothing of note other than /a/ card, giving the name of your friend, Francis. Not enough info yet.." << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day1 = true; 
+		}
 		choice();
 	}
 }
 
-void dayTwo() 
+void dayTwo()  //points user to day four, a failsafe in the beginning to ensure the finale plays at 0 for the global int
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -137,13 +152,18 @@ void dayTwo()
 		cout << "You did wonder if yu had misplaced your things when you got back, but now you see, it's future shenanigans. " << endl;
 		cout << "You disvover the remnants of what was, once, a rather important looking piece of cardstock, now eaten by one of your friends' dogs. " << endl;
 		cout << "If your memory serves you, the dog came over and tore it to shreds two days ago. " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day2 = true;
+		}
 		choice();
 	}
 }
 
-void dayThree()
+void dayThree() //gift wrapping
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -154,13 +174,18 @@ void dayThree()
 		cout << "Cute furball, needed a chew toy. Very bite-y. " << endl;
 		cout << "You may as well head out, too.  " << endl;
 		cout << "In your travels you find gift wrap fairly cheap, which you remember buying. So you were making some gift look pretty. " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day3 = true;
+		}
 		choice();
 	}
 }
 
-void dayFour()
+void dayFour() //pastry recipie
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -171,13 +196,18 @@ void dayFour()
 		cout << "Past you is asleep, however. you could warn yourself, but.. the dog was cute, and nothing major was damaged. " << endl;
 		cout << "A few bits of paper is nothing! Speaking of, in an inner monologue, you see a list of ingredients.  " << endl;
 		cout << "Flour, eggs, baking soda, cocoa powder.. definitely some sort of pastry recipe. " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day4 = true;
+		}
 		choice();
 	}
 }
 
-void dayFive()
+void dayFive() //gift
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -188,13 +218,18 @@ void dayFive()
 		cout << "Yeah, it was too snowy to go anywhere in short notice, but the slopes of white were a welcome sight. " << endl;
 		cout << "You also know where the dent on your lawn came from. You. " << endl;
 		cout << "Looking around inside afterward, and find a figurine from a game Francis loves. It's not near Christmas, nor was it on sale.. " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day5 = true;
+		}
 		choice();
 	}
 }
 
-void daySix()
+void daySix() //event plan
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -204,13 +239,18 @@ void daySix()
 		cout << "The day of Smores, where you also.. burned your calendar. As kindling... It should still be intact yesterday though. Hopefully. " << endl;
 		cout << "You see a list of places that your friends enjoy hanging out at. Party paces, or at least fun experience places. " << endl;
 		cout << "Definitely palnning some variety of event, but you suppose the idea was dumped. It looks like a very, very unfinished plan. " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day6 = true;
+		}
 		choice();
 	}
 }
 
-void daySeven()
+void daySeven() //the answer
 {
-	if (outOfTime == 0)
+	if (outOfTime < 0)
 	{
 		cout << "Your wrist time machine buzzes with what is definitely a 'no' buzz." << endl;
 		Finale();
@@ -221,15 +261,58 @@ void daySeven()
 		cout << "You removed the staples, too. Only paper went in. " << endl;
 		cout << "the urge to facepalm overwhelms you. really??? " << endl;
 		cout << "Written right on current day, Francis' birthday. How could you forget!?? " << endl;
+		if (outOfTime > -1)
+		{
+			outOfTime -= 1;
+			day7 = true;
+		}
 		choice();
 	}
 }
 
-void Finale() 
+void Finale() //resolution; going to add a check to see what numbers have been selected (using booleans) and from there offer clues rather than stating the fact.
 {
 	cout << "Drats! Time isn't on your side. Now you're being booted back to current day.." << endl;
 	cout << "Your head spins as you return to current day, your house. Rapid fire time travel doesn't feel good.." << endl;
-	cout << "With your evidence, however? Time to head to the store, you have to bake a cake for the birthday! " << endl;
+	cout << "With your evidence, what is happening today? " << endl;
+	if (day7 == true) //get it to stop saying 7 is true after the fact
+	{
+		cout << "Well, now you know you know. It's Francis' birthday!" << endl;
+	}
+	else
+	{
+		if (day1 == true)
+		{
+			cout << "You saw a decorative card..." << endl;
+		}
+		if (day2 == true)
+		{
+			if (day4 == true)
+			{
+				cout << "You saw what that paper was, thankfully." << endl;
+			}
+			else 
+			{
+				cout << "Some paper shreds... " << endl;
+			}
+		}
+		if (day3 == true)
+		{
+			cout << "Gift wrapping that you bought..." << endl;
+		}
+		if (day4 == true)
+		{
+			cout << "A recipie for some pastry..." << endl;
+		}
+		if (day5 == true)
+		{
+			cout << "A gift, presumably, based on what it was, for Francis..." << endl;
+		}
+		if (day6 == true)
+		{
+			cout << "Some plans for some event being made..." << endl;
+		}
+	}
 }
 
 int main()
@@ -260,6 +343,13 @@ int main()
 		else if (again == 'y')
 		{
 			outOfTime = 3;
+			day1 = false;
+			day2 = false;
+			day3 = false;
+			day4 = false;
+			day5 = false;
+			day6 = false;
+			day7 = false;
 		}
 		else
 		{
@@ -271,7 +361,7 @@ int main()
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 // .... .- .--. .--. -.-- / -.. . -.-. --- -.. .. -. --.
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
