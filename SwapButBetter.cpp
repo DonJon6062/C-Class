1f2225b0273de31a3a10fe4ef9e98d90916b9ea4
DonJon6062 <144158458+DonJon6062@users.noreply.github.com>
2024-07-29 22:28:05 -0700

    Add files via upload

---
+++ SwapButBetter.cpp
@@ -0,0 +1,126 @@
+// SwapButBetter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+void modifyValue(int* a, int*b);
+
+void modifyValue(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void modifyWord(string* a, string* b);
+
+void modifyWord(string* a, string* b)
+{
+    string temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+
+int main()
+{
+    int choice;
+    int secondChoice;
+    char playAgain;
+
+    int numUn;
+    int numDeux;
+
+    string first; 
+    string second;
+
+    while (true)
+    {
+
+        cout << "Enter a number." << endl;
+        cin >> numUn;
+        cout << "Enter another number." << endl;
+        cin >> numDeux;
+        cout << "You entered " << numUn << " and " << numDeux <<  endl;
+        cout << "Would you like to see something cool?" << endl;
+        cout << "Press 1." << endl;
+        cin >> choice;
+        if (choice == 1)
+        {
+            modifyValue(&numUn, &numDeux);
+            cout << "Now the first value is " << numUn << endl;
+            cout << "And the second value is " << numDeux << endl;
+        }
+        else
+        {
+            while (choice != 1)
+            {
+                cout << "Please cooperate. Press number 1, as displayed on your keyboard." << endl;
+                cin >> choice;
+                if (choice == 1)
+                {
+                    modifyValue(&numUn, &numDeux);
+                    cout << "Now the first value is " << numUn << endl;
+                    cout << "And the second value is " << numDeux << endl;
+                }
+            }
+        }
+        cout << "Enter a color." << endl;
+        cin >> first;
+        cout << "Enter a shape." << endl;
+        cin >> second;
+        cout << "You entered " << first << " and " << second << endl;
+        cout << "One more time!" << endl;
+        cout << "Press 1." << endl;
+        cin >> secondChoice;
+        if (secondChoice == 1)
+        {
+            modifyWord(&first, &second);
+            cout << "Now the first value is " << first << endl;
+            cout << "And the second value is " << second << endl;
+        }
+        else
+        {
+            while (secondChoice != 1)
+            {
+                cout << "Please cooperate. Press number 1, as displayed on your keyboard." << endl;
+                cin >> secondChoice;
+                if (secondChoice == 1)
+                {
+                    modifyWord(&first, &second);
+                    cout << "Now the first value is " << first << endl;
+                    cout << "And the second value is " << second << endl;
+                }
+            }
+        }
+        cout << "Do it again? Y or N." << endl;
+        cin >> playAgain;
+        if (playAgain == 'N')
+        {
+            break;
+        }
+        else if (playAgain == 'n')
+        {
+            break;
+        }
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
